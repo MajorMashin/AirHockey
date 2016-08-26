5ce43dcbffaa10740e67a7acbafc225fd597d64a
deXterbed <manojmishra.deb@gmail.com>
2016-08-26 18:29:18 +0530

    v4

--- app/src/main/java/com/mdsft/airhockey1/AirHockeyRenderer.java
+++ app/src/main/java/com/mdsft/airhockey1/AirHockeyRenderer.java
@@ -18,7 +18,7 @@ import static android.opengl.GLES20.*;
 import static android.opengl.Matrix.orthoM;
 
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
-    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int POSITION_COMPONENT_COUNT = 4;
     private static final int COLOR_COMPONENT_COUNT = 3;
     private static final int BYTES_PER_FLOAT = 4;
 
@@ -44,20 +44,20 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         this.context = context;
         float[] tableVerticesWithTriangles = {
             // Triangle Fan
-            0f,    0f,    1f,   1f,   1f,
-            -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
-            0.5f,  -0.8f, 0.7f, 0.7f, 0.7f,
-            0.5f,  0.8f,  0.7f, 0.7f, 0.7f,
-            -0.5f, 0.8f,  0.7f, 0.7f, 0.7f,
-            -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+               0f,    0f, 0f, 1.5f,   1f,   1f,   1f,
+            -0.5f, -0.8f, 0f,   1f, 0.7f, 0.7f, 0.7f,
+             0.5f, -0.8f, 0f,   1f, 0.7f, 0.7f, 0.7f,
+             0.5f,  0.8f, 0f,   2f, 0.7f, 0.7f, 0.7f,
+            -0.5f,  0.8f, 0f,   2f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.8f, 0f,   1f, 0.7f, 0.7f, 0.7f,
 
             // Line 1
-            -0.5f, 0f, 1f, 0f, 0f,
-            0.5f, 0f, 1f, 0f, 0f,
+            -0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
+             0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
 
             // Mallets
-            0f, -0.25f, 0f, 0f, 1f,
-            0f, 0.25f, 1f, 0f, 0f
+            0f, -0.25f, 0f, 1.25f, 0f, 0f, 1f,
+            0f,  0.25f, 0f, 1.75f, 1f, 0f, 0f
         };
 
         vertexData = ByteBuffer.allocateDirect
