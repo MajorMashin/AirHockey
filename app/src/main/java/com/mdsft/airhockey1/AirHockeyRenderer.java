73718199676d9c36e5cbfc26a61397a1b5ab6d69
deXterbed <manojmishra.deb@gmail.com>
2016-08-28 21:06:12 +0530

    v7

--- app/src/main/java/com/mdsft/airhockey1/AirHockeyRenderer.java
+++ app/src/main/java/com/mdsft/airhockey1/AirHockeyRenderer.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 
 import com.mdsft.airhockey1.objects.Mallet;
+import com.mdsft.airhockey1.objects.Puck;
 import com.mdsft.airhockey1.objects.Table;
 import com.mdsft.airhockey1.programs.ColorShaderProgram;
 import com.mdsft.airhockey1.programs.TextureShaderProgram;
@@ -20,8 +21,13 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     private final float[] projectionMatrix = new float[16];
     private final float[] modelMatrix = new float[16];
 
+    private final float[] viewMatrix = new float[16];
+    private final float[] viewProjectionMatrix = new float[16];
+    private final float[] modelViewProjectionMatrix = new float[16];
+
     private Table table;
     private Mallet mallet;
+    private Puck puck;
 
     private TextureShaderProgram textureProgram;
     private ColorShaderProgram colorProgram;
@@ -39,7 +45,8 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
         table = new Table();
-        mallet = new Mallet();
+        mallet = new Mallet(0.08f, 0.15f, 32);
+        puck = new Puck(0.06f, 0.02f, 32);
 
         textureProgram = new TextureShaderProgram(context);
         colorProgram = new ColorShaderProgram(context);
@@ -55,13 +62,7 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         MatrixHelper.perspectiveM(projectionMatrix, 45,
             (float) width / (float) height, 1f, 10f);
 
-        setIdentityM(modelMatrix, 0);
-        translateM(modelMatrix, 0, 0f, 0f, -2.6f);
-        rotateM(modelMatrix, 0, -60f, 1f, 0f, 0f);
-
-        final float[] temp = new float[16];
-        multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0);
-        System.arraycopy(temp, 0, projectionMatrix, 0, temp.length);
+        setLookAtM(viewMatrix, 0, 0f, 1.3f, 2.2f, 0f, 0f, 0f, 0f, 1f, 0f);
     }
 
     @Override
@@ -69,16 +70,46 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         // Clear the rendering surface.
         glClear(GL_COLOR_BUFFER_BIT);
 
+        multiplyMM(viewProjectionMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+
         // Draw the table.
+        positionTableInScene();
         textureProgram.useProgram();
-        textureProgram.setUniforms(projectionMatrix, texture);
+        textureProgram.setUniforms(modelViewProjectionMatrix, texture);
         table.bindData(textureProgram);
         table.draw();
 
         // Draw the mallets.
+        positionObjectInScene(0f, mallet.height / 2f, -0.4f);
         colorProgram.useProgram();
-        colorProgram.setUniforms(projectionMatrix);
+        colorProgram.setUniforms(modelViewProjectionMatrix, 1f, 0f, 0f);
         mallet.bindData(colorProgram);
         mallet.draw();
+
+        positionObjectInScene(0f, mallet.height / 2f, 0.4f);
+        colorProgram.setUniforms(modelViewProjectionMatrix, 0f, 0f, 1f);
+        mallet.draw();
+
+        // Draw the puck.
+        positionObjectInScene(0f, puck.height / 2f, 0f);
+        colorProgram.setUniforms(modelViewProjectionMatrix, 0.8f, 0.8f, 1f);
+        puck.bindData(colorProgram);
+        puck.draw();
+    }
+
+    private void positionTableInScene() {
+    // The table is defined in terms of X & Y coordinates, so we rotate it
+    // 90 degrees to lie flat on the XZ plane.
+        setIdentityM(modelMatrix, 0);
+        rotateM(modelMatrix, 0, -90f, 1f, 0f, 0f);
+        multiplyMM(modelViewProjectionMatrix, 0, viewProjectionMatrix,
+            0, modelMatrix, 0);
+    }
+
+    private void positionObjectInScene(float x, float y, float z) {
+        setIdentityM(modelMatrix, 0);
+        translateM(modelMatrix, 0, x, y, z);
+        multiplyMM(modelViewProjectionMatrix, 0, viewProjectionMatrix,
+            0, modelMatrix, 0);
     }
 }
--- app/src/main/java/com/mdsft/airhockey1/objects/Puck.java
+++ app/src/main/java/com/mdsft/airhockey1/objects/Puck.java
@@ -1,24 +1,21 @@
 package com.mdsft.airhockey1.objects;
 
 import com.mdsft.airhockey1.data.VertexArray;
+import static com.mdsft.airhockey1.objects.ObjectBuilder.*;
 import com.mdsft.airhockey1.programs.ColorShaderProgram;
 import com.mdsft.airhockey1.util.Geometry;
 
-import static com.mdsft.airhockey1.objects.ObjectBuilder.*;
-
 import java.util.List;
 
-public class Mallet {
+public class Puck {
     private static final int POSITION_COMPONENT_COUNT = 3;
-
-    public final float radius;
-    public final float height;
+    public final float radius, height;
     private final VertexArray vertexArray;
     private final List<DrawCommand> drawList;
 
-    public Mallet(float radius, float height, int numPointsAroundMallet) {
-        GeneratedData generatedData = ObjectBuilder.createMallet(
-            new Geometry.Point(0f, 0f, 0f), radius, height, numPointsAroundMallet);
+    public Puck(float radius, float height, int numPointsAroundPuck) {
+        GeneratedData generatedData = ObjectBuilder.createPuck(new Geometry.Cylinder(
+            new Geometry.Point(0f, 0f, 0f), radius, height), numPointsAroundPuck);
         this.radius = radius;
         this.height = height;
         vertexArray = new VertexArray(generatedData.vertexData);
@@ -26,9 +23,8 @@ public class Mallet {
     }
 
     public void bindData(ColorShaderProgram colorProgram) {
-        vertexArray.setVertexAttribPointer(0,
-        colorProgram.getPositionAttributeLocation(),
-        POSITION_COMPONENT_COUNT, 0);
+        vertexArray.setVertexAttribPointer(0, colorProgram.getPositionAttributeLocation(),
+            POSITION_COMPONENT_COUNT, 0);
     }
 
     public void draw() {
